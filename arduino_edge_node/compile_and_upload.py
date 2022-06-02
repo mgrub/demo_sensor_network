fb7b4420d1c42fbbe329a4c91cc1fc54eaea7ecb
Maximilian Gruber <maximilian.gruber@ptb.de>
2022-06-02 17:34:06 +0200

    make compile+upload script compatible with linux
    also fix correct use of C++ raw string

--- arduino_edge_node/compile_and_upload.py
+++ arduino_edge_node/compile_and_upload.py
@@ -1,20 +1,26 @@
-import sys
 import os
 import subprocess
 import shlex
-import shutil
 import json
 
+# paths windows
+#arduino_cli_path = "C:\\Users\\gruber04\\Downloads\\arduino-cli_0.21.1_Windows_64bit\\arduino-cli.exe"
+#work_dir = ".\\arduino_edge_node\\"
+#posix = False
+
+# paths linux
+arduino_cli_path = os.path.expanduser("~/programs/arduino-cli_0.22.0_Linux_64bit/arduino-cli")
+work_dir = "./arduino_edge_node/"
+posix = True
+
 # commands
-arduino_cli_path = "C:\\Users\\gruber04\\Downloads\\arduino-cli_0.21.1_Windows_64bit\\arduino-cli.exe"
-compilation = "{EXE} compile -b arduino:samd:nano_33_iot .\\arduino_edge_node\\"
-upload = "{EXE} upload -b arduino:samd:nano_33_iot -p {PORT} .\\arduino_edge_node\\"
+compilation = "{EXE} compile -b arduino:samd:nano_33_iot {WORK_DIR}"
+upload = "{EXE} upload -b arduino:samd:nano_33_iot -p {PORT} {WORK_DIR}"
+
 
 # template for sensors.h
 with open("arduino_edge_node/sensor_template.h", "r") as f:
     sensors_header_file_template = f.read()
-
-print(sensors_header_file_template)
 
 # get sensors to
 sensors_to_work_with = json.load(open("arduino_edge_node/sensors.json", "r"))
@@ -25,6 +31,7 @@ for sensor_id, settings in sensors_to_work_with.items():
     if os.path.exists(settings["description_file"]):
         with open(settings["description_file"], "r") as f:
             sensor_description = f.read()
+            sensor_description = sensor_description.replace("\n", " \\\n")
     else:
         continue
 
@@ -36,14 +43,21 @@ for sensor_id, settings in sensors_to_work_with.items():
     # create sensors.h
     with open("arduino_edge_node/sensor.h", "w") as f:
         sensor_description = f.write(sensor_header)
+        f.close()
 
     # compile
-    cmd = shlex.split(compilation.format(EXE=arduino_cli_path), posix=False)
-    #subprocess.run(cmd, capture_output=True)
+    cmd = shlex.split(compilation.format(EXE=arduino_cli_path, WORK_DIR=work_dir), posix=posix)
+    print(" ".join(cmd))
+    result = subprocess.run(cmd, capture_output=True)
+    print(result.stderr)
+    print(result.stdout)
 
     # (try to) upload
-    cmd = shlex.split(upload.format(EXE=arduino_cli_path, PORT=settings["port"]), posix=False)
-    #subprocess.run(cmd, capture_output=True)
+    cmd = shlex.split(upload.format(EXE=arduino_cli_path, WORK_DIR=work_dir, PORT=settings["port"]), posix=posix)
+    print(" ".join(cmd))
+    result = subprocess.run(cmd, capture_output=True)
+    print(result.stderr)
+    print(result.stdout)
 
     # clean up
     os.remove("arduino_edge_node/sensor.h")
